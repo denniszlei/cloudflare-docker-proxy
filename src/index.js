a111cab2b54fa39fb22a5002ac5f53cd32d366cd
denniszlei <124473644+denniszlei@users.noreply.github.com>
2024-09-09 15:49:25 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zlei.top": dockerHub,
+  "quay.zlei.top": "https://quay.io",
+  "gcr.zlei.top": "https://gcr.io",
+  "k8s-gcr.zlei.top": "https://k8s.gcr.io",
+  "k8s.zlei.top": "https://registry.k8s.io",
+  "ghcr.zlei.top": "https://ghcr.io",
+  "cloudsmith.zlei.top": "https://docker.cloudsmith.io",
+  "ecr.zlei.top": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
